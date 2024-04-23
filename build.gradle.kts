cca3fdfcf828fde1d7ae22169565dbd13f687b10
Claudio Cordero <cordero.guiza@gmail.com>
2024-04-23 11:51:49 +0200

    conflict merge 1

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,8 +12,8 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
-    implementation("com.fasterxml.jackson.core:jackson-core:2.16.2")
-	implementation("com.fasterxml.jackson.core:jackson-databind:2.16.2")  
+    implementation("com.fasterxml.jackson.core:jackson-core:2.16.1")
+	implementation("com.fasterxml.jackson.core:jackson-databind:2.16.1")  
 }
 
 tasks.test {
