4ce82219a429409a41e801686f7215d696331f3f
Carla Roca <159011993+caroga87@users.noreply.github.com>
2024-04-29 10:27:11 +0200

    Nou codi MySQL

--- src/main/java/n2MySQL/MySQLdatabase/FlowerSQL.java
+++ src/main/java/n2MySQL/MySQLdatabase/FlowerSQL.java
@@ -2,11 +2,7 @@ package n2MySQL.MySQLdatabase;
 
 import n2MySQL.DAO.FlowerDAO;
 import n2MySQL.beans.Flower;
-import n2MySQL.utils.Input;
-
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.List;
 
 public class FlowerSQL implements FlowerDAO {
@@ -17,33 +13,68 @@ public class FlowerSQL implements FlowerDAO {
 
     @Override
     public void create(Flower flower) {
-        String name = Input.inputString("Flower's name:");
-        String colour = Input.inputString("Colour:");
-        double price = Input.inputDouble("Flower's price:");
-        try (PreparedStatement st = connection.prepareStatement(MySQLQueries.INSERT_FLOWER)){
-            st.setString(1, name);
-            st.setString(2, colour);
-            st.setDouble(3, price);
-            st.setInt(4, flower.getIdProduct() );
-            int rowsAffected = st.executeUpdate();
 
-            if (rowsAffected > 0) {
-                System.out.println("Decoración insertada correctamente."); // modificar en ingles y en otra clase :)
-            } else {
-                System.out.println("No se pudo insertar la decoración.");
+        try {
+            PreparedStatement productst = connection.prepareStatement(MySQLQueries.INSERT_PRODUCT, Statement.RETURN_GENERATED_KEYS);
+            PreparedStatement flowerst = connection.prepareStatement((MySQLQueries.INSERT_FLOWER));
+            productst.setString(1, flower.getName());
+            productst.setDouble(2, flower.getSellPrice());
+            productst.setDouble(3, flower.getCostPrice());
+            productst.setInt(4, flower.getStock());
+            productst.setString(5, "Flower");
+            productst.executeUpdate();
+
+            try (ResultSet rs = productst.getGeneratedKeys()) {
+                int product_id = -1;
+                if (rs.next()) {
+                    product_id = rs.getInt(1);
+                    flowerst.setInt(1, product_id);
+                    flowerst.setString(2, flower.getColour());
+                    flowerst.executeUpdate();
+
+                }
+
             }
+            System.out.println("Product was added successfully");
         } catch (SQLException e) {
-            System.out.println("Error al insertar la decoración: " + e.getMessage());
+            e.printStackTrace();
         }
     }
 
     @Override
-    public void update(Flower object) {
+    public void update(Flower flower) {
+        try {
+            PreparedStatement flowerst = connection.prepareStatement(MySQLQueries.UPDATE_FLOWER);
+            flowerst.setString(1,flower.getName());
+            flowerst.setString(2, flower.getColour());
+            flowerst.setDouble(3, flower.getSellPrice());
+            flowerst.setDouble(4, flower.getCostPrice());
+            flowerst.setInt(5, flower.getProductId());
+            flowerst.executeUpdate();
+        }catch (SQLException e) {
+            e.printStackTrace();
+        }
 
     }
 
     @Override
-    public void delete(Flower object) {
+    public void delete(Flower flower) {
+        try {
+            PreparedStatement flowerst = connection.prepareStatement(MySQLQueries.DELETE_FLOWER);
+            PreparedStatement productst = connection.prepareStatement(MySQLQueries.DELETE_PRODUCT);
+            flowerst.setInt(1, flower.getProductId());
+            flowerst.executeUpdate();
+
+            productst.setInt(1, flower.getProductId());
+            int rowsAffected = productst.executeUpdate();
+            if (rowsAffected >0) {
+                System.out.println("Flower deleted successfully");
+            }else {
+                System.out.println("The product ID does not find ");
+            }
+        }catch (SQLException e) {
+            e.printStackTrace();
+        }
 
     }
 
--- src/main/java/n2MySQL/MySQLdatabase/MySQLQueries.java
+++ src/main/java/n2MySQL/MySQLdatabase/MySQLQueries.java
@@ -2,11 +2,16 @@ package n2MySQL.MySQLdatabase;
 
 public class MySQLQueries {
 
-    public static final String INSERT_FLOWER = "INSERT INTO flowers (name, colour, price, products_product_id) VALUES (?, ?, ?, ?)";
-    public static final String INSERT_DECORATION= "INSERT INTO decorations (name, material, price, products_product_id) VALUES (?, ?, ?, ?)";
-    public static final String INSERT_SALE = "INSERT INTO sales (time_created, total_paid) VALUES (?, ?)";
+    public static final String INSERT_PRODUCT = "INSERT INTO product (name, sell_price, cost_price, stock, type) VALUES (?,?,?,?,?)";
+    public static final String INSERT_FLOWER = "INSERT INTO flower (flower_id, colour) VALUES (?, ?)";
+    public static final String INSERT_DECORATION= "INSERT INTO decoration (decoration_id, material) VALUES (?, ?)";
+    public static final String INSERT_TREE ="INSERT INTO tree (tree_id, height) VALUES (?,?)";
+    public static final String DELETE_PRODUCT = "DELETE FROM product WHERE product_id = (?)";
+    public static final String DELETE_FLOWER = "DELETE FROM flower WHERE product_id = (?)";
+    public static final String DELETE_DECORATION = "DELETE FROM decoration WHERE product_id = (?)";
+    public static final String DELETE_TREE = "DELETE FROM tree WHERE product_id = (?)";
     public static final String UPDATE_DECORATION = "UPDATE decorations SET name = ?, material = ?, price = ? WHERE decoration_id = ?";
-    public static final String DELETE_PRODUCT = "DELETE FROM products WHERE product_id = (?)";
+    public static final String UPDATE_FLOWER = "UPDATE flower SET name =?, colour=?, sell_price=?, cost_price=? WHERE flower_id=?";
     public static final String GET_ALL_FLOWERS = "SELECT * FROM flowers";
     public static final String GET_ALL_PRODUCTS = "SELECT * FROM products";
     public static final String GET_ALL_TREES = "SELECT * FROM trees";
